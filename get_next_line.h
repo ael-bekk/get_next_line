3df1b432923c48083f8914580e0997d6841e4c7a
Abdellah El bekkali <ael-bekk@e1r13p6.1337.ma>
2021-11-23 14:07:00 +0100

    gnl

---
+++ get_next_line.h
@@ -0,0 +1,31 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   get_next_line.h                                    :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: ael-bekk <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2021/11/22 15:20:19 by ael-bekk          #+#    #+#             */
+/*   Updated: 2021/11/23 13:45:20 by ael-bekk         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef GET_NEXT_LINE_H
+# define GET_NEXT_LINE_H
+
+# include <stdlib.h>
+# include <unistd.h>
+# include <limits.h>
+# define TRUE 1
+
+void	*ft_free(char **s);
+char	*read_buff(int fd, char **buff);
+char	*read_line(int fd, char **buff);
+char	*get_next_line(int fd);
+
+int		find_new_line(char *s);
+char	*ft_strjoin(char *s1, char *s2);
+char	*ft_substr(char *s, int end);
+void	ft_new_buff(char **buff, int start);
+
+#endif
